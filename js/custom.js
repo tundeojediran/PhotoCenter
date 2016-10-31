8810a9b6d67715cb3eb71963663b4c8eb8d599e2
dannytee <tundeojediran@gmail.com>
2016-10-31 08:56:12 +0100

    first PhotoCenter code commit

---
+++ js/custom.js
@@ -0,0 +1,42 @@
+/**
+ * Created by dannytee on 10/30/16.
+ */
+
+// Application ID
+var CLIENT_ID = "1d87ef7b607505e13278b219ad0af0b589a89f53d807b91eb02bd562506f3474";
+
+//
+// function getPhotos() {
+//     $(function () {
+//
+//         $.ajax({
+//             type: 'GET',
+//             url: 'https://api.unsplash.com/photos?client_id='+CLIENT_ID,
+//             success: function (data) {
+//                 console.log('success', data);
+//             }
+//         });
+//     });
+//
+// }
+
+
+$(function () {
+
+    var $holder = $('#holder');
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.unsplash.com/photos?client_id='+CLIENT_ID,
+        success: function (photos) {
+            $.each(photos, function (i, photo) {
+                $holder.append("<section class='col-md-6 col-md-offset-3'><section class='content'>" +
+                    "<img class='img-responsive' src='"+photo.urls.full+"'> " +
+                    "</section></section>" );
+
+            });
+            // console.log('success', photos);
+
+        }
+    });
+});
